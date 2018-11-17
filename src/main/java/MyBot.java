db26ddaf9a3da0d4549daa5d32681aab6cc120a3
Celmas <ilnursup@mail.ru>
2018-11-17 22:57:31 +0300

    mirror bot

---
+++ src/main/java/MyBot.java
@@ -0,0 +1,35 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class MyBot extends TelegramLongPollingBot {
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage()){
+            Message message = update.getMessage();
+
+            //check if the message has text. it could also  contain for example a location ( message.hasLocation() )
+            if(message.hasText()){
+
+                //create a object that contains the information to send back the message
+                SendMessage sendMessageRequest = new SendMessage();
+                sendMessageRequest.setChatId(message.getChatId().toString()); //who should get the message? the sender from which we got the message...
+                sendMessageRequest.setText("you said: " + message.getText());
+                try {
+                    execute(sendMessageRequest); //at the end, so some magic and send the message ;)
+                } catch (TelegramApiException e) {
+                    //do some error handling
+                }//end catch()
+            }//end if()
+        }//end  if()
+    }
+
+    public String getBotUsername() {
+        return "YouAreAwesome";
+    }
+
+    public String getBotToken() {
+        return "661693148:AAFPnVd-XR_mdcW8w7tYCx748bh9yDOL2mg";
+    }
+}
